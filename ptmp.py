bac47306fe46aa88aa9c72e1a08e1b83a7bfcff5
Brett Viren <brett.viren@gmail.com>
2019-06-05 16:05:41 -0400

    Finder for ptmp, add waf 2.0.17

---
+++ ptmp.py
@@ -0,0 +1,9 @@
+import generic
+
+def options(opt):
+    generic._options(opt, 'ptmp')
+
+def configure(cfg):
+    generic._configure(cfg, 'ptmp', incs=('ptmp/api.h',), libs=('ptmp',),
+                       pcname='libptmp',
+                       uses = 'LIBPTMP', mandatory=True)
